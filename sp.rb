2f7570317a4522d4c8646e6081309042fb86d294
Aaron Thompson <acthompson.w@gmail.com>
2016-02-22 09:37:53 -0800

    Idiot-proofing

--- sp.rb
+++ sp.rb
@@ -1,8 +1,9 @@
-puts "Gimme a word."
+puts "Gimme a word. "
 user_input = gets.chomp
 
 # if user_input's first letter is "p," then add "s" to the front and assign it to new variable word.
 # if user_input's first letter is not "p," then add "sp" to the front and assign it to new variable word.
+# if user_input's first letter is not a letter, then puts "fuck off mate"
 
 word.upcase!
 
