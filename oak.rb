97120cff8c669c8181b696157e64551fb4abb54b
Aaron Thompson <acthompson.w@gmail.com>
2016-02-22 05:01:46 -0800

    Create oak.rb
    Gives an error message and exits (too quickly to read the error message!).

---
+++ oak.rb
@@ -0,0 +1,42 @@
+puts "Hello there! Welcome to the world of POKéMON!"
+sleep(3)
+
+puts "My name is OAK! People call me the POKéMON PROF! "
+sleep(4)
+
+puts "This world is inhabited by creatures called POKéMON!"
+sleep(3.5)
+
+puts "For some people, POKéMON are pets. Others use them for fights."
+sleep(4)
+
+puts "Myself..."
+sleep(2)
+
+puts "I study POKéMON as a profession."
+sleep(3)
+
+print "First, what is your name?"
+first_name = gets.chomp
+first_name.upcase
+
+puts "Right! So your name is #{name}!"
+sleep(3)
+
+puts "This is my grand-son. He's been your rival since you were a baby."
+sleep(4)
+
+print "...Erm, what is his name again?"
+rival_name = gets.chomp
+rival_name.upcase
+
+puts "That's right! I remember now! His name is #{rival_name}!"
+sleep(4)
+
+puts "#{name}!"
+sleep(2)
+
+puts "Your very own POKéMON legend is about to unfold!"
+sleep(4)
+
+puts "A world with dreams and adventures with POKéMON awaits! Let's go!"
