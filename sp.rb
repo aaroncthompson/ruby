a7ed537d56f2c8e1f7480bc633c88824a37c43c8
Aaron Thompson <acthompson.w@gmail.com>
2016-02-24 00:32:23 -0800

    Update sp.rb

--- sp.rb
+++ sp.rb
@@ -1,9 +1,12 @@
-puts "Gimme a word. "
+puts "gimme a word. "
 user_input = gets.chomp
+
+while (the first letter of user_input) == !(letter)
+  puts "for real, give me a goddang word. "
+  user_input = gets.chomp
 
 # if user_input's first letter is "p," then add "s" to the front and assign it to new variable word.
 # if user_input's first letter is not "p," then add "sp" to the front and assign it to new variable word.
-# if user_input's first letter is not a letter, then puts "fuck off mate"
 
 word.upcase!
 
